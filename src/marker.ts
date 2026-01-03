b7cf8b8de73e92fe415070879753a8ed51ddd1c2
Johan Book <13253042+johanbook@users.noreply.github.com>
2026-01-03 22:14:22 +0100

    refactor: clean up

--- src/marker.ts
+++ src/marker.ts
@@ -1,23 +1,38 @@
 import { BasesEntry } from "obsidian";
 
+const fillColor = "red";
+const size = "8";
+const strokeWidth = "2";
+
 export function renderMarker(
 	x: number,
 	y: number,
 	svgEl: SVGSVGElement,
 	item: BasesEntry,
 ): void {
-	const fillColor = "red";
-	const size = "16";
-
-	const circle = svgEl.createSvg("circle", {
+	svgEl.createSvg("circle", {
 		attr: {
 			cx: String(x),
 			cy: String(y),
 			r: size,
 			fill: fillColor,
+			stroke: "black",
+			"stroke-width": strokeWidth,
 		},
+		cls: "wb-marker",
 	});
 
-	const titleEl = circle.createSvg("title");
-	titleEl.textContent = title;
+	const title = item.getValue("file.name");
+
+	if (title) {
+		const titleEl = svgEl.createSvg("text", {
+			attr: {
+				x: String(x),
+				y: String(y) + 30,
+				fill: "black",
+			},
+			cls: "wb-marker-text",
+		});
+		titleEl.textContent = title.toString();
+	}
 }
